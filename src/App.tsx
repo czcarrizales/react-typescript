23ca14867f0c61bd5738f430e8980ce1e253b204
Chazz <czcarrizales@yahoo.com>
2023-02-27 20:34:53 -0800

    learning more typescript with react and models

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { InputField } from "./components/InputField";
+import { Todo } from "./model";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault()
+  }
 
   console.log(todo)
 
   return (
     <div className="App">
-        <InputField todo={todo} setTodo={setTodo} />
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -3,12 +3,13 @@ import React from "react";
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-export const InputField: React.FC<Props> = ({todo, setTodo}) => {
+export const InputField: React.FC<Props> = ({todo, setTodo, handleAdd}) => {
 
   return (
-    <form>
+    <form onSubmit={handleAdd}>
       <input type="text" placeholder="Enter task" value={todo} onChange={(e) => setTodo(e.target.value)} />
       <button type="submit">Go</button>
     </form>
